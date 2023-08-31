aa6c539aee0be58b1d47c1a4671840ffab6b2b24
taiane94sb <taiane19sb94@gmail.com>
2023-08-31 11:08:39 -0300

    Drawing A Snake

--- demo.py
+++ demo.py
@@ -9,9 +9,14 @@ window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Snake Game")
 
+white = (255, 255, 255)
+
 game_over = False
 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+    pygame.draw.rect(window, white, [400, 300, 10, 10])
+    pygame.display.update()
